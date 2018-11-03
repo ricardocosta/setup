8239fa51864d62ffa6d8ac1b7794dfa626b1b454
Ricardo Costa <ricardocosta1989@gmail.com>
2018-11-03 15:02:05 +0000

    Add script with general applications
    Add script that uses homebrew to install general purpose applications:
    - 1Password
    - Alfred
    - Google Chrome
    - Grammarly
    - Keybase
    - Rocket
    - QLStephen
    - Slack
    - Spectacle
    - Spotify
    - Typora
    - VLC
    - WhatsApp

---
+++ scripts/1_general.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+source "${BASH_SOURCE%/*}/functions.lib"
+
+print_start cask 1password
+install cask 1password
+
+print_start cask alfred
+install cask alfred
+
+print_start cask google-chrome
+install cask google-chrome
+
+print_start cask grammarly
+install cask grammarly
+
+print_start cask keybase
+install cask keybase
+
+print_start cask rocket
+install cask rocket
+
+print_start cask qlstephen
+install cask qlstephen
+
+print_start cask slack
+install cask slack
+
+print_start cask spectacle
+install cask spectacle
+
+print_start cask spotify
+install cask spotify
+
+print_start cask typora
+install cask typora
+
+print_start cask vlc
+install cask vlc
+
+print_start cask whatsapp
+install cask whatsapp
