df9f6a1882d6c5a5de8905809a77e9f63678469f
Ricardo Costa <ricardocosta1989@gmail.com>
2018-11-03 15:14:01 +0000

    Add script to install terminal-based tools
    Add a script that install tools to be used within the terminal
    application:
    - autojump
    - bat
    - csvkit
    - diff-so-fancy
    - fd
    - htop
    - httpie
    - jq
    - ncdu
    - noti
    - q
    - siege
    - the_silver_searcher
    - tldr
    - unrar
    - fzf

---
+++ scripts/5_terminal_tools.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+source "${BASH_SOURCE%/*}/functions.lib"
+
+print_start autojump
+install autojump
+
+print_start bat
+install bat
+
+print_start csvkit
+install csvkit
+
+print_start diff-so-fancy
+install diff-so-fancy
+
+print_start fd
+install fd
+
+print_start htop
+install htop
+
+print_start httpie
+install httpie
+
+print_start jq
+install jq
+
+print_start ncdu
+install ncdu
+
+print_start noti
+install noti
+
+print_start q
+install q
+
+print_start siege
+install siege
+
+print_start the_silver_searcher
+install the_silver_searcher
+
+print_start tldr
+install tldr
+
+print_start unrar
+install unrar
+
+print_start fzf
+install fzf
+$(brew --prefix)/opt/fzf/install
