afeba2ba9908b712122741f2cee9f3748e36c465
Ricardo Costa <ricardocosta1989@gmail.com>
2020-07-25 12:19:00 +0100

    Change order of theme setup

--- scripts/3_terminal_config.sh
+++ scripts/3_terminal_config.sh
@@ -11,15 +11,15 @@ install zsh
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo 'Installing spaceship prompt'
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+cp ../dotfiles/.theme ~/.theme
+echo 'source ~/.theme' >> ~/.zshrc
+
 cp ../dotfiles/.exports ~/.exports
 echo 'source ~/.exports' >> ~/.zshrc
 
 cp ../dotfiles/.aliases ~/.aliases
 echo 'source ~/.aliases' >> ~/.zshrc
-
-cp ../dotfiles/.theme ~/.theme
-echo 'source ~/.theme' >> ~/.zshrc
-
-echo 'Installing spaceship prompt'
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
