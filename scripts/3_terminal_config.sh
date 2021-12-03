c8ca3a3cebf44e949f45d3bda900e722a7b5ba27
Ricardo Costa <ricardocosta1989@gmail.com>
2021-12-03 22:22:01 +0000

    Remove zsh
    Zsh is now the default on macOS, so it's not needed anymore.

--- scripts/3_terminal_config.sh
+++ scripts/3_terminal_config.sh
@@ -5,9 +5,6 @@ source "${BASH_SOURCE%/*}/functions.lib"
 print_start cask iterm2
 install cask iterm2
 
-print_start zsh
-install zsh
-
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
