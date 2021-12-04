7702310e54be02545690decf620acdbbcc52cf7d
Ricardo Costa <ricardocosta1989@gmail.com>
2021-12-04 19:54:49 +0000

    Update terminal tools
    Remove tools that I rarely use and add 'entr' to watch modified files
    and run commands on them.

--- scripts/5_terminal_tools.sh
+++ scripts/5_terminal_tools.sh
@@ -8,9 +8,6 @@ install autojump
 print_start bat
 install bat
 
-print_start csvkit
-install csvkit
-
 print_start diff-so-fancy
 install diff-so-fancy
 
@@ -23,20 +20,11 @@ install fd
 print_start htop
 install htop
 
-print_start httpie
-install httpie
-
 print_start jq
 install jq
 
 print_start ncdu
 install ncdu
-
-print_start noti
-install noti
-
-print_start q
-install q
 
 print_start the_silver_searcher
 install the_silver_searcher
@@ -44,14 +32,8 @@ install the_silver_searcher
 print_start tldr
 install tldr
 
-print_start vegeta
-install vegeta
-
-print_start watch
-install watch
-
-print_start unrar
-install unrar
+print_start entr
+install entr
 
 print_start fzf
 install fzf
