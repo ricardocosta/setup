af551431a06fc56398470e99cd1b4cdf4e41f335
Ricardo Costa <ricardocosta1989@gmail.com>
2021-12-07 18:01:10 +0000

    Update frontend dev script
    Remove yarn as it is now available via corepack (installed with Node).
    Also remove Figma as I'm not really using it.

--- scripts/6_dev_frontend.sh
+++ scripts/6_dev_frontend.sh
@@ -5,14 +5,8 @@ source "${BASH_SOURCE%/*}/functions.lib"
 echo "Installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
-print_start yarn
-install yarn
-
 print_start cask firefox
 install cask firefox
 
 print_start cask google-chrome
 install cask google-chrome
-
-print_start cask figma
-install cask figma
